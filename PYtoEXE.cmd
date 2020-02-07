e7c10dc8244c10f2c239400137678594dd4b1a2b
吴亦泓 <47708677+shangkelingxiang@users.noreply.github.com>
2020-02-07 21:10:36 +0800

    Delete PYtoEXE.cmd

--- PYtoEXE.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p name=
-pyinstaller -F %name%.py
-del %name%.spec
-rmdir __pycache__ /s/q
-rmdir build /s/q
-if exist %name%.exe del %name%.exe
-copy dist\%name%.exe %name%.exe
-rmdir dist /s/q
